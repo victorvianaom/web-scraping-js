d9542fbb4316f1a3708731ca32e05f928d9d89be
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-07-19 13:38:07 -0300

    cheerio, source code professro site

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const http = require('http');
 const PORT = 3000;
 
-// this way of working with axios is the recomended, because the above is still in level three of formal acceptance    
+// this way of working with axios is the recomended, because the above is still in level three of formal acceptance
+const axios = require('axios')    
 async function getQuestions() {
     try {
         const response = await axios.get(
             'http://professor.bio.br/quimica/lista.all.asp?curpage=4'
         )
-        console.log(response)
+        //console.log(response)
+        const cheerio = require('cheerio')
+        const $ = cheerio.load(response.data)
+        console.log($('body'))
+        //return response
     } catch (error) {
         console.error(error)
     }
 }
 getQuestions()
+
+// cheerio
+
+// $('h2.title').text('Hello there!')
+// $('h2').addClass('welcome')
+// $.html()
+
+
+
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
