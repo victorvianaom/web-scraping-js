8a4c8554ff5310a5d78ad468db3211f9002205ae
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-07-19 12:58:53 -0300

    separated in tests and production

--- server.js
+++ server.js
@@ -1,26 +1,6 @@
 const http = require('http');
 const PORT = 3000;
 
-////////////////////////////////////////////Request (HTTP Client):
-// const request = require('request')
-// request('http://professor.bio.br/quimica/lista.all.asp?curpage=4', function (
-//   error,
-//   response,
-//   body
-// ) {
-//   console.error('error:', error)
-//   console.log('body:', body)
-// })
-////////////////////////////////////////////Axios (HTTP Client):
-// const axios = require('axios') ///the axios response is better....
-// axios
-// 	.get('http://professor.bio.br/quimica/lista.all.asp?curpage=4')
-// 	.then((response) => {
-// 		console.log(response)
-// 	})
-// 	.catch((error) => {
-// 		console.error(error)
-//     });
 // this way of working with axios is the recomended, because the above is still in level three of formal acceptance    
 async function getQuestions() {
     try {
@@ -33,38 +13,6 @@ async function getQuestions() {
     }
 }
 getQuestions()
-
-////////////////////////////////////////////SuperAgent (HTTP Client):
-const superagent = require("superagent")
-const forumURL = "https://www.reddit.com/r/programming.json"
-
-// callbacks
-superagent
-	.get(forumURL)
-	.end((error, response) => {
-		console.log(response)
-	})
-
-// promises
-superagent
-	.get(forumURL)
-	.then((response) => {
-		console.log(response)
-	})
-	.catch((error) => {
-		console.error(error)
-	})
-
-// promises with async/await
-async function getForum() {
-	try {
-		const response = await superagent.get(forumURL)
-		console.log(response)
-	} catch (error) {
-		console.error(error)
-	}
-}
-
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
